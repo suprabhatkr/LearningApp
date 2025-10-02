46f3e987ef826b0a24c19421b3ef97a839269ece
Suprabhat Kumar <jnvsuprabh@gmail.com>
2025-10-03 00:17:13 +0530

    Learning App with Button, List View, TextField and Navigation View.

--- LearningApp/ContentView.swift
+++ LearningApp/ContentView.swift
@@ -7,15 +7,57 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Song: Identifiable {
+    var id = UUID()
+    var title: String
+    var artist: String
+}
+
+struct SongDetailView: View {
+    var song: Song
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(song.title)
+            Text(song.artist)
+        }
+    }
+}
+
+struct ContentView: View {
+    @State var counter = 0
+    @State var name = ""
+    @State var email = ""
+    var songs = [
+        Song(title: "Photograph", artist: "Ed Sheeren"),
+        Song(title: "Baby", artist: "Just Beiber"),
+        Song(title: "Despacito", artist: "Louis Fonci"),
+        Song(title: "Hips Don't lie", artist: "Shakira")
+    ]
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("You have tapped on the button \(counter) times")
+            Button("Tap Me") {
+                self.counter += 1
+            }
+            Button("Reset") {
+                self.counter = 0
+            }
         }
         .padding()
+        NavigationView {
+            List(songs) {
+                song in VStack {
+                    NavigationLink(destination: SongDetailView(song: song)) {
+                        Text(song.title).font(.title)
+                    }
+                }
+            }
+        }
+        VStack(spacing: 20) {
+            TextField("Enter your Name : ", text: $name).border(.blue)
+            TextField("Enter your Email : ", text: $email).border(.blue)
+            Text("The email address of \(name) is \(email)")
+        }
     }
 }
 
